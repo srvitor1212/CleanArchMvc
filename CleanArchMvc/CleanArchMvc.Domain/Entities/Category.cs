b6f4ced9489bac99fac9aadb003a374bb348cdb3
Vitor - PC <vitor.email2011@gmail.com>
2022-11-24 00:37:30 -0300

    Entidades e validações

---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    // sealed não pode ser herdada
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+                
+        // 1:N de Product
+        public ICollection<Product> Products { get; private set; }
+
+
+        public Category(string name)
+        {
+            ValidadeDomain(name);
+        }
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0,
+                "Id inválido");
+
+            ValidadeDomain(name);
+
+            this.Id = id;
+        }
+
+
+        private void ValidadeDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Nome inválido. Nome é obrigatório");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Nome inválido. Minimo de 3 caracteres");
+
+            this.Name = name;
+        }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    // sealed não pode ser herdada
+    public sealed class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        // 1:1 de Category
+        public int CategoryId { get; private set; }
+        public Category Category { get; private set; }
+    }
+}
