85ef2cc8e8355e916433b010bb6bb1d043402b2a
Vitor - PC <vitor.email2011@gmail.com>
2022-11-26 10:06:44 -0300

    Interfaces do Product e Category

---
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        // Task = Assíncrono
+
+        // UmaListaDe<Produtos>
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<IEnumerable<Product>> GetProductCategoryAsync();
+
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
