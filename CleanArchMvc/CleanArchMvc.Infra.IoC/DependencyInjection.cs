a714e19b4ea619f81ff9336efcd257cc5f8b48a2
Vitor Vicente <vitor.email2011@gmail.com>
2022-12-01 13:28:28 -0300

    Criado injeção de dependência na Infra ioc

---
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+
+        }
+    }
+}
