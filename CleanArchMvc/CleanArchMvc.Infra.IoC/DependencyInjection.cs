7683fb44fff9acb7c95d4f9ca3bab8c7a8107582
Vitor Vicente <vitor.email2011@gmail.com>
2022-12-01 15:19:48 -0300

    Finalizando injeção de dependência na Infra ioc

--- CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArchMvc.Infra.IoC
@@ -9,7 +13,15 @@ namespace CleanArchMvc.Infra.IoC
             this IServiceCollection services, 
             IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
         }
     }
 }
