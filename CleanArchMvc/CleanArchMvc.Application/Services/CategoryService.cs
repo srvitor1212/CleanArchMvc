93e77adfdc29b28323a1b6c8c0650500656ec676
Vitor Vicente <vitor.email2011@gmail.com>
2023-01-25 15:38:40 -0300

    Serviço de category

---
+++ CleanArchMvc/CleanArchMvc.Application/Services/CategoryService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+
+        public CategoryService(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IEnumerable<CategoryDTO>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CategoryDTO> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Add(CategoryDTO categoryDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(CategoryDTO categoryDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Remove(int? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
