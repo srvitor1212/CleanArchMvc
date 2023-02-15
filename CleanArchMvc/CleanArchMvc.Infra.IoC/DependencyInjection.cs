18e1a2da6b8e9877eb16d964b1716757c19d9986
Vitor Vicente <vitor.email2011@gmail.com>
2023-02-15 11:51:24 -0300

    Parte 4 - Injeção de dependência

--- CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc/CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -4,6 +4,7 @@ using CleanArchMvc.Application.Services;
 using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,9 @@ namespace CleanArchMvc.Infra.IoC
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+            services.AddMediatR(myhandlers);
 
             return services;
         }
