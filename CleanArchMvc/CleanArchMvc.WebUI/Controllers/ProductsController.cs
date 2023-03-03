6d15b6516b0e5b75f69ff574008282cd5ee67229
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-03 15:58:43 -0300

    Create Produtos, Viewbag para select de categorias

--- CleanArchMvc/CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,15 +1,19 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
 
@@ -19,5 +23,25 @@ namespace CleanArchMvc.WebUI.Controllers
             var products = await _productService.GetProducts();
             return View(products);
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId =
+                new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO produtoDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.Add(produtoDTO);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
     }
 }
