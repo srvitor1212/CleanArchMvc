97c952cc9564dcc862c0ca309152769d7a178c97
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-10 15:08:21 -0300

    Criando service de autenticação

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Identity/AuthenticateService.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchMvc.Infra.Data.Identity
+{
+    public class AuthenticateService : IAuthenticate
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public Task<bool> Authenticate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterUser(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
