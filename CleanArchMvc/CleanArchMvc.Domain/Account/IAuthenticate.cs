bb15cb0cde03d91eb931477aa682a26691697a45
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-10 15:02:58 -0300

    Definições da interface

--- CleanArchMvc/CleanArchMvc.Domain/Account/IAuthenticate.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Account/IAuthenticate.cs
@@ -2,5 +2,8 @@
 {
     public interface IAuthenticate
     {
+        Task<bool> Authenticate(string email, string password);
+        Task<bool> RegisterUser(string email, string password);
+        Task Logout();
     }
 }
