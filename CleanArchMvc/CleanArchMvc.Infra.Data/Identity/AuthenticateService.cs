bf5b8caef96f799273c741446c7374bb2f689ea3
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-10 15:14:07 -0300

    Authenticate e RegisterUser

--- CleanArchMvc/CleanArchMvc.Infra.Data/Identity/AuthenticateService.cs
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Identity/AuthenticateService.cs
@@ -8,14 +8,37 @@ namespace CleanArchMvc.Infra.Data.Identity
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public Task<bool> Authenticate(string email, string password)
+        public AuthenticateService(
+            UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager)
         {
-            throw new NotImplementedException();
+            _userManager = userManager;
+            _signInManager = signInManager; 
         }
 
-        public Task<bool> RegisterUser(string email, string password)
+        public async Task<bool> Authenticate(string email, string password)
         {
-            throw new NotImplementedException();
+            var result = await _signInManager.PasswordSignInAsync(
+                email, password, false, lockoutOnFailure: false);
+
+            return result.Succeeded;
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var applicationUser = new ApplicationUser
+            {
+                UserName = email,
+                Email = email,
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(applicationUser, isPersistent: false);
+            }
+            return result.Succeeded;
         }
 
         public Task Logout()
