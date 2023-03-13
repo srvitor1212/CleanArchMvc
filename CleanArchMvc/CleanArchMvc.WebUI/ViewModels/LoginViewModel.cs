858220d93a4ec0b24daf508efe04c4ad6be18eae
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-13 11:47:35 -0300

    LoginViewModel

---
+++ CleanArchMvc/CleanArchMvc.WebUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.WebUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email obrigatório")]
+        [EmailAddress(ErrorMessage = "Email incorreto")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha obrigatória")]
+        [StringLength(20, ErrorMessage = "A {0} precisa ter entre {2} e {1} de tamanho.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
