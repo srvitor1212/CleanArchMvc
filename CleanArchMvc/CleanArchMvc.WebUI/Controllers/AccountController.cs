c06203bfdce918018160b7fbc1b063ece14ff785
Vitor Vicente <vitor.email2011@gmail.com>
2023-03-20 14:38:13 -0300

    Login, Register Logout -  Controller

--- CleanArchMvc/CleanArchMvc.WebUI/Controllers/AccountController.cs
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using CleanArchMvc.Domain.Account;
+using CleanArchMvc.WebUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -11,5 +12,64 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             _authenticate = authenticate;
         }
+
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authenticate.Authenticate(model.Email, model.Password);
+
+            if (result)
+            {
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Email ou senha inválidos!");
+                return View(model);
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var result = await _authenticate.RegisterUser(model.Email, model.Password);
+
+            if (result)
+            {
+                return Redirect("/");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Não foi possível registrar-se");
+                return View(model);
+            }
+        }
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await _authenticate.Logout();
+            return Redirect("/Account/Login");
+        }
+
     }
 }
