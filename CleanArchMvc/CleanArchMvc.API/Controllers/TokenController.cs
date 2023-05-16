f9975eecd56c2e9c840819590877eba26c9586f8
Vitor Vicente <vitor.email2011@gmail.com>
2023-05-16 13:21:11 -0300

    Token Controller

---
+++ CleanArchMvc/CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.API.Models;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public TokenController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate ?? 
+                throw new ArgumentNullException(nameof(authenticate));
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
